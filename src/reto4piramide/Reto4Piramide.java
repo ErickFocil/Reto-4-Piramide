44db05e52c9eab88aed6dd05735fa2cfea5ac392
Erick Focil <edfocilg@hotmail.com>
2019-02-12 08:23:23 -0600

    Creacion de proyecto y peticion del tamaño del aprimanide

---
+++ src/reto4piramide/Reto4Piramide.java
@@ -0,0 +1,19 @@
+package reto4piramide;
+
+import java.util.Scanner;
+
+public class Reto4Piramide {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String lectura;
+        int pirTam;
+        
+        System.out.println("Idique el tamaño de la piramide: ");
+        lectura = entrada.next();
+        pirTam = Integer.parseInt(lectura);
+        
+        
+    }
+    
+}
