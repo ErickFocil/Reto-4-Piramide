6e2197f36d2b0610e6c3c64cdd6405c4d59d57a9
Erick Focil <edfocilg@hotmail.com>
2019-02-12 10:23:15 -0600

    Acomodo y dibujo de la piramide funcional

--- src/reto4piramide/Reto4Piramide.java
+++ src/reto4piramide/Reto4Piramide.java
@@ -7,11 +7,29 @@ public class Reto4Piramide {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         String lectura;
+        String espacios = "";
+        String asteriscos = "*";
         int pirTam;
         
         System.out.println("Idique el tamaño de la piramide: ");
         lectura = entrada.next();
         pirTam = Integer.parseInt(lectura);
+        
+        for(int i = 0; i < pirTam; i++){
+            
+            for(int j = 0; j < pirTam - i; j++){
+                espacios += " ";
+            }
+            
+            for(int j = 0; j < i; j++){
+                asteriscos += "**";
+            }
+            
+            System.out.println(espacios + asteriscos);
+            
+            espacios = "";
+            asteriscos = "*";
+        }
         
         
     }
